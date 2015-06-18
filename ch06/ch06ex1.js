089761ca0e9dada4c47f0d378a4012d2c68cec04
Andy <andygnewman@gmail.com>
2015-06-18 14:02:11 +0100

    implement own basic promises - working

--- ch06/ch06ex1.js
+++ ch06/ch06ex1.js
@@ -27,6 +27,7 @@ var PromiseMe = function() {
 
       this.myState = newState;
       this.value = newValue;
+      this.resolve();
       return this.myState;
     },
 
@@ -61,29 +62,28 @@ var PromiseMe = function() {
 
       while(this.handlers && this.handlers.length) {
         var handler = this.handlers.shift();
-      }
 
-      var doResolve = (this.myState == promState.fulfilled ?
-        handler.fulfillPromise : handler.rejectPromise);
+        var doResolve = (this.myState == promState.fulfilled ?
+          handler.fulfillPromise : handler.rejectPromise);
 
-      if (typeof doResolve !== 'function') {
-        handler.promise.changeMyState(this.myState, this.value);
-      }
-      else {
-        try {
-          var promiseValue = doResolve(this.value);
-          if (promiseValue && typeof promiseValue.then === 'function') {
-            promiseValue.then(function(val) {
-              handler.promise.changeMyState(promState.fulfilled, val);
-            }, function(error) {
-              handler.promise.changeMyState(promState.rejected, error);
-            });
+        if (typeof doResolve !== 'function') {
+          handler.promise.changeMyState(this.myState, this.value);
+        }
+        else {
+          try {
+            var promiseValue = doResolve(this.value);
+            if (promiseValue && typeof promiseValue.then === 'function') {
+              promiseValue.then(function(val) {
+                handler.promise.changeMyState(promState.fulfilled, val);
+              }, function(error) {
+                handler.promise.changeMyState(promState.rejected, error);
+              });
+            } else {
+              handler.promise.changeMyState(promState.fulfilled, promiseValue);
+            }
+          } catch (error) {
+            handler.promise.changeMyState(promState.rejected, error);
           }
-          else {
-            handler.promise.changeMyState(promState.fulfilled, promiseValue);
-          }
-        } catch (error) {
-          handler.promise.changeMyState(promState.rejected, error);
         }
       }
     }
@@ -92,3 +92,44 @@ var PromiseMe = function() {
 
   return Object.create(PromiseMe);
 };
+
+
+var multiplyMeAsync = function(val) {
+  var promise = new PromiseMe();
+  if (typeof val !== 'number') {
+    promise.rejectPromise("Not a number");
+  }
+  else {
+    promise.fulfillPromise(val * 2);
+  }
+  return promise;
+};
+
+var divideAsync = function(val) {
+  console.log("divideAsync being called");
+  var promise2 = new PromiseMe();
+  if (val === 0) {
+    console.log("being rejected");
+    promise2.rejectPromise("Cannot divide by 0");
+  }
+  else {
+    console.log("being fulfilled");
+    promise2.fulfillPromise(1 / val);
+  }
+  return promise2;
+};
+
+var alertResult = function(value) {
+  alert("Result is: " + value);
+};
+
+var onError = function(error) {
+  alert("An error was encountered: " + error);
+};
+
+multiplyMeAsync(2).
+  then(alertResult, onError);
+
+multiplyMeAsync(0).
+  then(divideAsync).
+  then(alertResult, onError);
